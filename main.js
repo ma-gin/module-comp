88cc482e46fbca56292cbc34ef308d4300a871f7
Matt Ginter <mattginter@gmail.com>
2021-12-28 19:01:55 +0100

    Ex 13 - 14

--- main.js
+++ main.js
@@ -14,6 +14,40 @@ const removeItem = (e) => {
     e.target.parentNode.remove()
 }
 
+const addStudent = () => {
+    const table = document.getElementById('studentTable')
+    const newRow = document.createElement('tr')
+    let newTd = document.createElement('td')
+    const studentID = document.getElementById('ID')
+    newTd.innerHTML = studentID.value
+    newRow.appendChild(newTd)
+    newTd = document.createElement('td')
+    const studentName = document.getElementById('studentName')
+    newTd.innerHTML = studentName.value
+    newRow.appendChild(newTd)
+    newTd = document.createElement('td')
+    const studentSurname  = document.getElementById('surname')
+    newTd.innerHTML = studentSurname.value
+    newRow.appendChild(newTd)
+    newTd = document.createElement('td')
+    const studentAge = document.getElementById('age')
+    newTd.innerHTML = studentAge.value
+    newRow.appendChild(newTd)
+    newTd = document.createElement('td')
+    const studentEmail = document.getElementById('email')
+    newTd.innerHTML = studentEmail.value
+    newRow.appendChild(newTd)
+    table.appendChild(newRow)
+
+}
+
+const addDeleteBtn = () => {
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerText = "Remove"
+    deleteBtn.setAttribute('onclick', 'removeItem(event)')
+    return deleteBtn
+}
+
 
 const addItem = () => {
     const listItem = document.createElement('div')
@@ -22,8 +56,6 @@ const addItem = () => {
     listItem.style.border = "1px solid red"
     enrolled.appendChild(listItem)
     listItem.appendChild(itemContent)
-    const deleteBtn = document.createElement('button')
-    deleteBtn.innerText = "Remove"
-    deleteBtn.setAttribute('onclick', 'removeItem(event)')
+    const deleteBtn = addDeleteBtn()
     listItem.appendChild(deleteBtn)
 }
