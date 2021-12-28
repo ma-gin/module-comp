7fbe4e82bc05ebe14de0216231ed82ea9c7d68b1
Matt Ginter <mattginter@gmail.com>
2021-12-28 13:04:21 +0100

    Ex 6 - 12 done

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 const input = document.getElementById('textChange')
+const enrolled = document.getElementById('enrolledContainer')
 
 const textChange = () => {
-    document.getElementById('pageHeader').innerText = input.value
-    document.title = input.value
+    if (!input.value) {
+        alert('You forgot to write something.')
+    } else {
+        document.getElementById('pageHeader').innerText = input.value
+        document.title = input.value
+    }
+}
+
+const removeItem = (e) => {
+    e.target.parentNode.remove()
+}
+
+
+const addItem = () => {
+    const listItem = document.createElement('div')
+    const itemContent = document.createElement('p')
+    listItem.style.backgroundColor = "blue"
+    listItem.style.border = "1px solid red"
+    enrolled.appendChild(listItem)
+    listItem.appendChild(itemContent)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerText = "Remove"
+    deleteBtn.setAttribute('onclick', 'removeItem(event)')
+    listItem.appendChild(deleteBtn)
 }
